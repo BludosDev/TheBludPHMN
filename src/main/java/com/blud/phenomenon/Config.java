6d16a6184d63512e3fbe6a02219e22b809811b8a
BludosDev <d1zxyblud@gmail.com>
2024-08-06 13:30:41 +0530

    Update Config.java

--- src/main/java/com/blud/phenomenon/Config.java
+++ src/main/java/com/blud/phenomenon/Config.java
@@ -13,10 +13,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-// Config class for Blud Phenomenon mod.
-// Demonstrates how to use Forge's config APIs.
-@Mod.EventBusSubscriber(modid = BludPhenomenon.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class Config {
+@Mod.EventBusSubscriber(modid = MainMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config
+{
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
     private static final ForgeConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
@@ -31,7 +30,7 @@ public class Config {
             .comment("What you want the introduction message to be for the magic number")
             .define("magicNumberIntroduction", "The magic number is... ");
 
-    // A list of strings that are treated as resource locations for items.
+    // a list of strings that are treated as resource locations for items
     private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
             .comment("A list of items to log on common setup.")
             .defineListAllowEmpty(Collections.singletonList("items"), () -> List.of("minecraft:iron_ingot"), Config::validateItemName);
@@ -43,17 +42,19 @@ public class Config {
     public static String magicNumberIntroduction;
     public static Set<Item> items;
 
-    private static boolean validateItemName(final Object obj) {
+    private static boolean validateItemName(final Object obj)
+    {
         return obj instanceof final String itemName && ForgeRegistries.ITEMS.containsKey(new ResourceLocation(itemName));
     }
 
     @SubscribeEvent
-    static void onLoad(final ModConfigEvent event) {
+    static void onLoad(final ModConfigEvent event)
+    {
         logDirtBlock = LOG_DIRT_BLOCK.get();
         magicNumber = MAGIC_NUMBER.get();
         magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
 
-        // Convert the list of strings into a set of items.
+        // convert the list of strings into a set of items
         items = ITEM_STRINGS.get().stream()
                 .map(itemName -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName)))
                 .collect(Collectors.toSet());
