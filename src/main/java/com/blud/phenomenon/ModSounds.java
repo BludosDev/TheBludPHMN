43c3a21e0682f4a7887a64f33735942544299cd2
BludosDev <d1zxyblud@gmail.com>
2024-08-09 20:38:26 +0530

    Update ModSounds.java

--- src/main/java/com/blud/phenomenon/ModSounds.java
+++ src/main/java/com/blud/phenomenon/ModSounds.java
@@ -2,11 +2,8 @@ package com.blud.phenomenon;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -17,6 +14,9 @@ public class ModSounds {
 
     public static final RegistryObject<SoundEvent> PHEN_228_SOUND = SOUND_EVENTS.register("phen_228", 
         () -> new SoundEvent(new ResourceLocation(MainMod.MODID, "phen_228")));
+
+    public static final RegistryObject<SoundEvent> BLOCKBREAK_SOUND = SOUND_EVENTS.register("blockbreak", 
+        () -> new SoundEvent(new ResourceLocation(MainMod.MODID, "blockbreak")));
 
     public static void register(IEventBus eventBus) {
         SOUND_EVENTS.register(eventBus);
